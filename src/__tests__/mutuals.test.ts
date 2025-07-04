1300bbbf342d8fc738956cec151122af8becf76a
artlu99 <alu99.sm@gmail.com>
2025-07-04 18:26:56 -0400

    clarify tests for mutuals

--- src/__tests__/mutuals.test.ts
+++ src/__tests__/mutuals.test.ts
@@ -2,15 +2,15 @@ import { describe, expect, it } from "bun:test";
 import { Elysia } from "elysia";
 import { mutuals } from "../routes/live";
 
-describe("Stats Routes", () => {
+describe("Mutuals Routes", () => {
     const app = new Elysia().use(mutuals);
 
-    describe("should get mutuals", () => {
+    describe("should check mutuals", () => {
         const testCases = [
-            { fid: 5650, expectedMutualsAtLeast: 109 }, // vitalik
-            { fid: 37, expectedMutualsAtLeast: 53 }, // balajis
-            { fid: 2, expectedMutualsAtLeast: 1000 }, // varun
-            { fid: 3, expectedMutualsAtLeast: 5200 }, // dwr
+            { fid: 5650, expectedMutualsAtLeast: 100 }, // vitalik
+            { fid: 37, expectedMutualsAtLeast: 50 }, // balajis
+            { fid: 2, expectedMutualsAtLeast: 1400 }, // varun
+            { fid: 3, expectedMutualsAtLeast: 3700 }, // dwr
         ];
 
         for (const { fid, expectedMutualsAtLeast } of testCases) {
